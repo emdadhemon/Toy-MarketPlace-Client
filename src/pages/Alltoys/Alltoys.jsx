857e0500c3d9e229474fba87457ef20709cd9ae4
emdadhemon <emdadhossainemon@gmail.com>
2023-05-20 11:44:16 +0600

    handle search done

--- src/pages/Alltoys/Alltoys.jsx
+++ src/pages/Alltoys/Alltoys.jsx
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import Alltoytabular from './Alltoytabular';
 
 const Alltoys = () => {
-    const toys = useLoaderData();
+    // const toys = useLoaderData();
+    const [toys, settoys] = useState([]);
+    const [searchText, setSearchText] = useState("");
+    useEffect(() => {
+        fetch(`http://localhost:5000/toys`)
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            settoys(data);
+          });
+      }, []);
+
+      const handleSearch = () => {
+        fetch(`http://localhost:5000/searchtoysByText/${searchText}`)
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            settoys(data);
+          });
+      };
+
     console.log(toys)
     return (
         <div className='w-[80%] mx-auto mt-12'>
             <div className='mb-4'>
-                <input className='input w-[35%] bg-white text-black mr-2' type="text" name="name" placeholder='Search Your Product' id="" />
-                <input className='btn ' type="submit" value="Search" />
+                <input onChange={(e) => setSearchText(e.target.value)} className='input w-[35%] bg-white text-black mr-2' type="text" name="name" placeholder='Search Your Product' id="" />
+                <input onClick={handleSearch} className='btn ' type="submit" value="Search" />
             </div>
             <div>
                 {
--- src/pages/MyToys/MyToys.jsx
+++ src/pages/MyToys/MyToys.jsx
@@ -2,18 +2,42 @@ import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../Providers/AuthProvider';
 
 const MyToys = () => {
-    const {user} = useContext(AuthContext)
+    const { user } = useContext(AuthContext)
 
     const [toys, setToys] = useState([])
-    useEffect(()=>{
+    useEffect(() => {
         fetch(`http://localhost:5000/mytoys/${user?.email}`)
-        .then(res => res.json())
-        .then(data => console.log(data))
-    },[])
-    
+            .then(res => res.json())
+            .then(data => console.log(data))
+    }, [user])
+
     return (
         <div>
-            
+            <div className="overflow-x-auto">
+                <table className="table w-full">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Job</th>
+                            <th>Favorite Color</th>
+                        </tr>
+                    </thead>
+                    <tbody>{
+                        toys.map(toy => 
+                            <tr>
+                                <th>1</th>
+                                <td>Cy Ganderton</td>
+                                <td>Quality Control Specialist</td>
+                                <td>Blue</td>
+                            </tr>
+
+                        )
+                    }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
